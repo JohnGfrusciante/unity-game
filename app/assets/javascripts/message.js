8d9d7ca0f9b1f5db9b44a31df865fe03c35c944b
JohnGfrusciante <g.rejime.frusciante@gmail.com>
2020-01-30 20:12:57 +0900

    message.jsのテンプレリテラルのインデント修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,29 +1,29 @@
 $(function(){
   function buildHTML(post){
     var html = `
-                  <li class="messagePost">
-                  <div class="messagePost__userName">
-                  ${post.user_name}
-                  </div>
-                  <div class="messagePost__date">
-                  ${post.created_at}
-                  </div>
-                  <div class="messagePost__message">
-                  ${post.text}
-                  </div>
-                  <div class="messagePost__likeCount">
-                  いいね件数 : 0
-                  </div>
-                  <div class="messagePost__lower">
-                  <div class="messagePost__lower__edit">
-                  <a data-method="get" href="/messages/${post.message_id}/edit">編集</a>
-                  </div>
-                  <div class="messagePost__lower__delete">
-                  <a rel="nofollow" data-method="delete" href="/messages/${post.message_id}">削除</a>
-                  </div>
-                  </div>
-                  </li>
-                  `
+                <li class="messagePost">
+                <div class="messagePost__userName">
+                ${post.user_name}
+                </div>
+                <div class="messagePost__date">
+                ${post.created_at}
+                </div>
+                <div class="messagePost__message">
+                ${post.text}
+                </div>
+                <div class="messagePost__likeCount">
+                いいね件数 : 0
+                </div>
+                <div class="messagePost__lower">
+                <div class="messagePost__lower__edit">
+                <a data-method="get" href="/messages/${post.message_id}/edit">編集</a>
+                </div>
+                <div class="messagePost__lower__delete">
+                <a rel="nofollow" data-method="delete" href="/messages/${post.message_id}">削除</a>
+                </div>
+                </div>
+                </li>
+                `
     return html;
   }
   $('#new_message').on('submit', function(e){
